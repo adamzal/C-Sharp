19044eff1cf016bee83939fe1da051ffbb36bcd8
Adam Zalewski <adam.zalewski1999@o2.pl>
2020-08-19 13:35:12 +0200

    Commit file Cwiczenie_7.1.cs

---
+++ Cwiczenia_7/Cwiczenie_7.1.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int a,b,c,wynik;
+		a=14;
+		b=5;
+		c=3;
+		wynik=a%b%c;
+		Console.WriteLine(wynik);
+		Console.ReadKey();
+	}
+}
