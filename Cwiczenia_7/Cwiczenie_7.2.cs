296003969cc7c2686589431d4d9bc15ae5eff583
Adam Zalewski <adam.zalewski1999@o2.pl>
2020-08-19 13:35:28 +0200

    Commit file Cwiczenie_7.2.cs

---
+++ Cwiczenia_7/Cwiczenie_7.2.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Program
+{
+	public static void Main()
+	{
+		int liczba=256;
+		int lpomoc=liczba;
+		Console.WriteLine(liczba>>=2);
+		Cosnole.WriteLine(lpomoc<<=2);
+		Console.ReadKey();
+	}
+}
