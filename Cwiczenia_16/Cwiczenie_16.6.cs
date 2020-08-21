eeabb6ed7c2a4422ea68d840eb5434042720a0ce
Adam Zalewski <adam.zalewski1999@o2.pl>
2020-08-22 00:46:28 +0200

    adding double bok and bool czypole

--- Cwiczenia_16/Cwiczenie_16.6.cs
+++ Cwiczenia_16/Cwiczenie_16.6.cs
@@ -7,66 +7,75 @@ class Punkt
 
 	class Kwadrat
 	{
-		int bok;
-		Punkt D=new Punkt();
-		Punkt B=new Punkt();
+		double bok;
+		Punkt D = new Punkt();
+		Punkt B = new Punkt();
 
 		int abs(int a)
 		{
-			return a>=0?a:-a;
+			return a >= 0 ? a : -a;
 		}
 
-		Kwadrat(Punkt D,Punkt B)
+		Kwadrat(Punkt D, Punkt B)
 		{
-			this.D=D;
-			this.B=B;
+			this.D = D;
+			this.B = B;
 		}
 
-		Kwadrat(Punkt D,int bok)
+		Kwadrat(Punkt D, double bok)
 		{
-			this.bok=bok;
-			this.D=D;
-			B.x=D.x+bok;
-			B.y=D.y-bok;
+			this.bok = bok;
+			this.D = D;
+			B.x = D.x + bok;
+			B.y = D.y - bok;
 		}
 
-		Kwadrat(int x,int y,int bok)
+		Kwadrat(double x, double y, double bok)
 		{
-			this.bok=bok
-			D.x=x+bok/2=D.y;
-			B.x=x-bok/2=B.y;
+			this.bok = bok;
+			D.x = D.y = x + bok / 2;
+			B.x = B.y = x - bok / 2;
 		}
 
-		Kwadrat(int x,int y,double pole)
+		Kwadrat(double x, double y, double pole, bool czypole)
 		{
-			this.bok=Math.Sqrt(pole);
-			D.y=D.x=x+bok/2;
-			B.y=B.x=x-bok/2;
+			if (czypole)
+			{
+				this.bok = Math.Sqrt(pole);
+				D.y = D.x = x + bok / 2;
+				B.y = B.x = x - bok / 2;
+			}
+			else
+			{
+				this.bok = bok;
+				D.x = D.y = x + bok / 2;
+				B.x = B.y = x - bok / 2;
+			}
 		}
 
 		void WyswietlWspolrzedne()
 		{
-			Console.WriteLine("B({0};{1})",B.x,B.y);
-                        Console.WriteLine("B({0};{1})",D.x,D.y);
+			Console.WriteLine("B({0};{1})", B.x, B.y);
+			Console.WriteLine("B({0};{1})", D.x, D.y);
 		}
 
 		public static void Main()
 		{
-	      		Punkt B = new Punkt();
-            		B.x = -4;
-		        B.y = 5;
-		        Punkt D = new Punkt();
-        		D.x = 0;
-        		D.y = 1;
-        		Kwadrat ABCD = new Kwadrat(D, B);
-        		ABCD.WyswietlWspolrzedne();
-        		Kwadrat abcd = new Kwadrat(D, 5);
-        		abcd.WyswietlWspolrzedne();
-        		Kwadrat EFGH = new Kwadrat(0,0, 6);
-        		EFGH.WyswietlWspolrzedne();
-        		Kwadrat efgh = new Kwadrat(0,0, 64.0);
-        		efgh.WyswietlWspolrzedne();
-		        Console.ReadKey();
+			Punkt B = new Punkt();
+			B.x = -4;
+			B.y = 5;
+			Punkt D = new Punkt();
+			D.x = 0;
+			D.y = 1;
+			Kwadrat ABCD = new Kwadrat(D, B);
+			ABCD.WyswietlWspolrzedne();
+			Kwadrat abcd = new Kwadrat(D, 5);
+			abcd.WyswietlWspolrzedne();
+			Kwadrat EFGH = new Kwadrat(0, 0, 6);
+			EFGH.WyswietlWspolrzedne();
+			Kwadrat efgh = new Kwadrat(0, 0, 64.0, true);
+			efgh.WyswietlWspolrzedne();
+			Console.ReadKey();
 		}
 	}
 }
