46481cdaab9bf8a5f9e38a29ed67626497eadd78
Adam Zalewski <adam.zalewski1999@o2.pl>
2020-09-16 19:08:19 +0200

    change class magazyn and produkt

--- PROJEKT/magazyn.cs
+++ PROJEKT/magazyn.cs
@@ -1,13 +1,10 @@
-using System;
-using System.IO;
-
 public class Magazyn
 {
-    private Object[][] produkt;
-    
+     Produkt[] produkt;
+
     public Magazyn()
     {
-        wczytaj();   
+        wczytaj();
     }
 
     private void wczytaj()
@@ -16,28 +13,18 @@ public class Magazyn
         string line = sr.ReadLine();
         if (line != null)
         {
-            produkt = new Object[Int32.Parse(line)][];
-            for (int i = 0; i < Int32.Parse(line); i++)
+            produkt = new Produkt[Int32.Parse(line)];
+            for(int i=0;i<produkt.Length&&line!=null;i++)
             {
-                produkt[i] = new Object[4];
-            }
-            line = sr.ReadLine();
-            int j = 0;
-            while (line != null)
-            {
-                string[] s = line.Split(new char[] { ';' });
-                produkt[j][0] = s[0];
-                produkt[j][1] = s[1];
-                produkt[j][2] = s[2];
-                produkt[j][3] = s[3];
-                j++;
                 line = sr.ReadLine();
+                string[] s = line.Split(new char[] { ';' });
+                produkt[i] = new Produkt(s[0], Int32.Parse(s[1]),s[2], Convert.ToDouble(s[3]));
             }
         }
         else
         {
-            produkt = new Object[1][];
-            produkt[0] = new Object[4];
+            produkt = new Produkt[1];
+            produkt[0] = new Produkt();
         }
         sr.Close();
     }
@@ -48,105 +35,112 @@ public class Magazyn
         sw.WriteLine(produkt.Length);
         for (int i = 0; i < produkt.Length; i++)
         {
-            sw.WriteLine(produkt[i][0] + ";" + produkt[i][1] + ";" + produkt[i][2] + ";" + produkt[i][3]);
+            sw.WriteLine(produkt[i].nazwa + ";" + produkt[i].ilosc + ";" + produkt[i].jednostka + ";" + produkt[i].cena);
         }
         sw.Close();
     }
 
-    public void usuńZMagazynu(string nazwa,int ilosc)
+    public void usunZMagazynu(string nazwa, int ilosc)
     {
-        for(int i=0;i<produkt.Length;i++)
+        for (int i = 0; i < produkt.Length; i++)
         {
-            if (Convert.ToString(produkt[i][0]) == nazwa)
+            if (produkt[i].nazwa == nazwa)
             {
                 if (ilosc < 0)
                 {
-                    Console.WriteLine("Podano ujemną wartość");
+                    Console.WriteLine("Podano ujemną ilość {0} produktu {1}",ilosc,nazwa);
                 }
                 else
                 {
-                    if (Convert.ToInt32(produkt[i][1]) - ilosc < 0)
+                    if (produkt[i].ilosc - ilosc < 0)
                     {
-                        Console.WriteLine("Na magazynie zostało {0} {1} {2} - nie możemy sprzedać {3}.", produkt[i][1], produkt[i][2], produkt[i][0], ilosc);
+                        Console.WriteLine("Na magazynie zostało {0} {1} {2} - nie możemy sprzedać {3}.", produkt[i].ilosc, produkt[i].jednostka, produkt[i].nazwa, ilosc);
                     }
                     else
                     {
-                        produkt[i][1] = Convert.ToInt32(produkt[i][1]) - ilosc;
+                        produkt[i].ilosc -= ilosc;
                         zapisz();
                     }
 
                 }
             }
+            else
+            {
+                Console.WriteLine("Produktu {0} nie ma na liście",nazwa);
+            }
         }
     }
 
-    public void dodajDoMagazynu(string nazwa, int ilosc,string jednostka,double cena)
-    {        
-        if(ilosc<0||cena<0)
+    public void dodajDoMagazynu(string nazwa, int ilosc, string jednostka, double cena)
+    {
+        if (ilosc < 0)
         {
-            Console.WriteLine("Podano ujemna wartosc");
+            Console.WriteLine("Podana ilość produktu {0} jest ujemna: {1}",nazwa,ilosc);
         }
         else
-        { 
-        StreamReader sr = new StreamReader(new FileStream("magazyn.txt", FileMode.OpenOrCreate));
-        string line = sr.ReadLine();
-            if (line == null)
+        {
+            if (cena < 0)
             {
-                sr.Close();
-                produkt[0][0] = nazwa;
-                produkt[0][1] = ilosc;
-                produkt[0][2] = jednostka;
-                produkt[0][3] = cena;
-                zapisz();
+                Console.WriteLine("Podana cena produktu {0} jest ujemna: {1}", nazwa, cena);
             }
             else
             {
-                sr.Close();
-                bool czyistnieje = false;
-                int a = 0;
-                while (!czyistnieje && a < produkt.Length)
+                StreamReader sr = new StreamReader(new FileStream("magazyn.txt", FileMode.OpenOrCreate));
+                string line = sr.ReadLine();
+                if (line == null)
                 {
-                    if (Convert.ToString(produkt[a][0]) == nazwa)
-                    {
-                        czyistnieje = true;
-                        produkt[a][1] = Convert.ToInt32(produkt[a][1]) + ilosc;
-                    }
-                    a++;
-                }
-                if (!czyistnieje)
-                {
-                    Object[][] obj = new Object[produkt.Length + 1][];
-                    for (int i = 0; i < produkt.Length + 1; i++)
-                    {
-                        obj[i] = new Object[4];
-                    }
-                    for (int i = 0; i < produkt.Length; i++)
-                    {
-                        for (int j = 0; j < 4; j++)
-                        {
-                            obj[i][j] = produkt[i][j];
-                        }
-                    }
-                    obj[produkt.Length][0] = nazwa;
-                    obj[produkt.Length][1] = ilosc;
-                    obj[produkt.Length][2] = jednostka;
-                    obj[produkt.Length][3] = cena;
-                    produkt = obj;
+                    sr.Close();
+                    produkt[0].nazwa = nazwa;
+                    produkt[0].ilosc = ilosc;
+                    produkt[0].jednostka = jednostka;
+                    produkt[0].cena = cena;
                     zapisz();
-                }                
+                }
+                else
+                {
+                    sr.Close();
+                    bool czyistnieje = false;
+                    int a = 0;
+                    while (!czyistnieje && a < produkt.Length)
+                    {
+                        if (produkt[a].nazwa == nazwa)
+                        {
+                            czyistnieje = true;
+                            produkt[a].ilosc += ilosc;
+                        }
+                        a++;
+                    }
+                    if (!czyistnieje)
+                    {
+                        Produkt[] pro = new Produkt[produkt.Length + 1];
+                        for (int i = 0; i < pro.Length; i++)
+                        {
+                            pro[i] = new Produkt();
+                        }
+                        for (int i = 0; i < produkt.Length; i++)
+                        {
+                            for (int j = 0; j < 4; j++)
+                            {
+                                pro[i] = produkt[i];
+                            }
+                        }
+                        pro[produkt.Length].nazwa = nazwa;
+                        pro[produkt.Length].ilosc = ilosc;
+                        pro[produkt.Length].jednostka = jednostka;
+                        pro[produkt.Length].cena = cena;
+                        produkt = pro;
+                    }
+                    zapisz();
+                }
             }
-        }                       
+        }
     }
 
     public void wyswietlMagazyn()
     {
-        for (int i=0;i<produkt.Length;i++)
-        {            
-            for(int j=0;j<4;j++)
-            {
-                Console.Write(produkt[i][j] + "\t");
-            }
-            Console.WriteLine("");
-        }
+        for (int i = 0; i < produkt.Length; i++)
+        {
+            produkt[i].ProduktInfo();
+        }        
     }
 }
--- PROJEKT/produkt.cs
+++ PROJEKT/produkt.cs
@@ -1,20 +1,63 @@
-using System;
-
 public class Produkt
 {
-    string nazwa;
-    int ilosc;
-    string jednostka;
-    double cena;
+    string _nazwa;
+    public string nazwa
+    {
+        get
+        {
+            return _nazwa;
+        }
+        set
+        {
+            _nazwa = value;
+        }
+    }
+    int _ilosc;
+    public int ilosc
+    {
+        get
+        {
+            return _ilosc;
+        }
+        set
+        {
+            _ilosc = value;
+        }
+    }
+    string _jednostka;
+    public string jednostka
+    {
+        get
+        {
+            return _jednostka;
+        }
+        set
+        {
+            _jednostka = value;
+        }
+    }
+    double _cena;
+    public double cena
+    {
+        get
+        {
+            return _cena;
+        }
+        set
+        {
+            _cena = value;
+        }
+    }
 
-    public Produkt(string nazwa,int ilosc,string jednostka,double cena)
+    public Produkt() { }
+    public Produkt(string nazwa, int ilosc, string jednostka, double cena)
     {
         this.nazwa = nazwa;
         this.ilosc = ilosc;
         this.jednostka = jednostka;
         this.cena = cena;
     }
-    
+
     public void ProduktInfo()
     {
         Console.WriteLine("Produkt: {0} \t Ilość: {1} {2} \t Cena: {3}", nazwa, ilosc, jednostka, cena);
